346a1deb77fbe507ed7a3eb910ab9ca578afacee
rebornweb <andrei.nicolas@hotmail.com>
2022-03-09 21:20:25 +1300

    answered macs

--- whitehat/network_scanner.py
+++ whitehat/network_scanner.py
@@ -4,7 +4,8 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
-    # scapy.ls(arp_request)
-
-scan("192.168.1.1/24")
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
+scan("Any ip on network range /24")
