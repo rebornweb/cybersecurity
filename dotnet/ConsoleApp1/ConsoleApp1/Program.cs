2e20a22f1ae3460621f7c7077ba0c108ee010fb5
Andrei Nicolas <andrei.nicolas@hotmail.com>
2022-05-12 05:04:43 +1200

    Tutorial

---
+++ dotnet/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("\nWhat is your name?");
+var name = Console.ReadLine();
+
+Console.WriteLine($"\nHello, {name}");
